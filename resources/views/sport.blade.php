5ea206c1dc57bb1cacf4e9e764a1804a34a2cc49
cookiepic <swiecki.kamil12@gmail.com>
2023-12-04 13:16:05 +0100

    part 1 of spr

---
+++ resources/views/sport.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+        <input type="text" name="name" placeholder="Imie"><br><br>
+        <input type="text" name="email" placeholder="Email"><br><br>
+        <input type="radio" name="color" value="red">Czerwony<br>
+        <input type="radio" name="color" value="zielony">Zielony<br>
+        <input type="radio" name="color" value="niebieski">Niebieski<br><br>
+        <input type="submit" value="Zatwiedź">
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,4 +26,8 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::get('zsk/sport/{sport}', function($sport){
+    return view('sport', ['sport'=>$sport]);
 });
